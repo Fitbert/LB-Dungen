d5447d134ef1e156bc7c4a14cb63f16aec765ddd
ceci-00 <ceci.ga003@gmail.com>
2024-05-30 19:12:41 -0500

    working on user schema model. set password validation and quizcount for user.

--- LB Dungen/server/models/User.js
+++ LB Dungen/server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     username: {
@@ -15,6 +16,34 @@ const userSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Quiz',
     }],
-    });
+    },
+{
+    toJSON: {
+        virtuals: true,
+    },
+}
+);
 
-module.exports = model('User', userSchema);
+// hash user password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const unique = 10;
+        this.password = await bcrypt.hash(this.password, unique);
+    }
+
+    next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+// when we query a user, we'll also get another field called `quizCount` with the number of saved quizzes we have
+userSchema.virtual('quizCount').get(function () {
+    return this.quizzes.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
