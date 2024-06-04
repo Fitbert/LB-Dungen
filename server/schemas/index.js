32c5b0702020e67818cab30857027651406644b8
ceci-00 <ceci.ga003@gmail.com>
2024-06-03 20:36:06 -0500

    re-formatted index.js in schemas to export typeDefs and resolvers

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,10 +1,4 @@
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import typeDefs from './typeDefs.js';
-import resolvers from '../resolvers/index.js';
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
 
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-});
-
-export { schema };
+module.exports = { typeDefs, resolvers };
