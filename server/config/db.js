8d5173c698de2d3b5a5968579fae2cc874e14105
ceci-00 <ceci.ga003@gmail.com>
2024-06-05 22:55:23 -0500

    changed import and export in config to connect to mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 // import dotenv from 'dotenv';
 
 // dotenv.config();
@@ -13,4 +13,4 @@ const connectDB = async () => {
   }
 };
 
-export default connectDB;
+module.exports = connectDB;
