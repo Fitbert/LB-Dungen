ee27f10f3a598fa0b1b092c5feefcbb6dffba75a
Albert T <albert.terrazas@gmail.com>
2024-06-09 17:25:12 -0500

    adjusted server for mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -3,22 +3,16 @@ const mongoose = require('mongoose');
 
 const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/languagedungeon';
 
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(dbURI)
+  .then(() => console.log('Mongoose is connected to', dbURI))
+  .catch((err) => {
+    console.error('Mongoose connection error:', err);
+    process.exit(1);  // Exit the process if the connection fails
+  });
+
 
 mongoose.connection.on('connected', () => {
   console.log('Mongoose is connected to', dbURI);
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error('Mongoose connection error:', err);
-  process.exit(1);  // Exit the process if the connection fails
-});
-
-mongoose.connection.on('disconnected', () => {
-  console.log('Mongoose is disconnected');
 });
 
 module.exports = mongoose.connection;
