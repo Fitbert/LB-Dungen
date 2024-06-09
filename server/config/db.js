5e12edcc2c4b16436af4eea83ad1b5612aa7a92c
Albert T <albert.terrazas@gmail.com>
2024-06-09 16:45:05 -0500

    fix db connection to mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,24 @@
 require("dotenv").config();
 const mongoose = require('mongoose');
 
-// dotenv.config();
+const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/languagedungeon';
 
-// const db = async () => {
-//   try {
-//     console.log('MongoDB connected');
-//     } catch (error) {
-//       console.error('MongoDB connection error:', error);
-//       process.exit(1);
-//       }
-//       };
-      
- mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/languagedungeon');
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected to', dbURI);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+  process.exit(1);  // Exit the process if the connection fails
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose is disconnected');
+});
+
 module.exports = mongoose.connection;
