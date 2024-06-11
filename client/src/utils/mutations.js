870cc2af702e20fce986c894f8c25094e183d05f
Albert T <albert.terrazas@gmail.com>
2024-06-11 16:11:28 -0500

    added queries and mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -10,4 +10,27 @@ mutation Mutation($username: String!, $password: String!) {
     }
   }
 }
+`;import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $password: String!, $avatar: String!) {
+    addUser(username: $username, password: $password, avatar: $avatar) {
+      token
+      user {
+        _id
+        username
+        avatar
+      }
+    }
+  }
+`;
+
+export const UPDATE_USER_SCORE = gql`
+  mutation updateUserScore($score: Int!) {
+    updateUserScore(score: $score) {
+      _id
+      username
+      points
+    }
+  }
 `;
