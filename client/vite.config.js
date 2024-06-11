f5ffca70bb5e34a4bb2b8c21075d891711074028
Albert T <albert.terrazas@gmail.com>
2024-06-11 15:34:15 -0500

    added index to public

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,29 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // Ensure the server binds to 0.0.0.0
-    port: 3000,       // Use the correct port
-    open: true,
-    proxy: {
-      '/graphql': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
+    port: 3000,
   },
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
-    outDir: 'dist',
-    sourcemap: false,
-  }
 });
