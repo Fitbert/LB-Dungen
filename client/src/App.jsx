f9d65e77cf730fc903d81b4346622621de4153e3
Albert T <albert.terrazas@gmail.com>
2024-06-11 15:44:21 -0500

    convert index main and app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,57 @@
-import './App.css';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
+import React from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { Outlet } from 'react-router-dom';
 
 import Header from './components/Header';
 import Cursor from './components/Cursor';
+import ErrorPage from './pages/ErrorPage.jsx';
+import HomePage from './pages/HomePage';
+import LeaderBoardPage from './pages/LeaderBoardPage.jsx';
+import QuizListPage from './pages/QuizListPage.jsx';
+import QuizDetailPage from './pages/QuizDetailPage.jsx';
+import SplashPage from './pages/SplashPage.jsx';
+import Donate from './pages/Donate.jsx';
 
-// Construct our main GraphQL API endpoint
+import './App.css';
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql',
 });
 
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
-      ...(headers || {}), // Ensure headers is defined before spreading its properties
+      ...headers,
       authorization: token ? `Bearer ${token}` : '',
     },
   };
 });
 
 const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
+  const location = useLocation();
+
   return (
     <ApolloProvider client={client}>
       <div className="flex-column justify-flex-start min-100-vh">
-          {window.location.pathname !== '/' && <Header />}
+        {location.pathname !== '/' && <Header />}
         <div className="container">
           <Cursor />
-          <Outlet />
+          <Routes>
+            <Route path="/" element={<SplashPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/leaderBoard" element={<LeaderBoardPage />} />
+            <Route path="/quiz" element={<QuizListPage />} />
+            <Route path="/quiz/:quizId" element={<QuizDetailPage />} />
+            <Route path="/donate" element={<Donate />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
         </div>
       </div>
     </ApolloProvider>
--- client/src/pages/SplashPage.jsx
+++ client/src/pages/SplashPage.jsx
@@ -1,44 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/main.css';
+import '../styles/SplashPage.css';
 import { ADD_USER } from '../utils/mutations';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
-import logo1000 from '/images/LD_Logo_1000px.png';
-// import './cursor.js'; //p Import cursor.js as a script
+import AvatarSelection from '../components/AvatarSelection';
 
 export default function SplashPage() {
   const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [addUser] = useMutation(ADD_USER);
+  const [avatar, setAvatar] = useState('');
+  const [addUser, { error }] = useMutation(ADD_USER);
 
-  // useEffect(() => {
-  //   const script = document.createElement('script');
-  //   script.src = cursorScript;
-  //   script.async = true;
-  //   document.body.appendChild(script);
-
-  //   return () => {
-  //     document.body.removeChild(script);
-  //   };
-  // }, []);
+  useEffect(() => {
+    if (Auth.loggedIn()) {
+      navigate('/home');
+    }
+  }, [navigate]);
 
   const handleSignUpClick = async () => {
-    // Perform sign-up logic with the username and password
-    console.log('Sign Up:', username, password);
-    // navigate('/signup');
-    const { data } = await addUser({ variables: { username, password } });
-    Auth.login(data.addUser.token);
+    try {
+      const { data } = await addUser({ variables: { username, password, avatar } });
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error('Error signing up:', e);
+    }
   };
 
   return (
-    <>
-    { Auth.loggedIn() ? navigate('/home') : (
     <div className="splash-page">
       <h1>Welcome to</h1>
-      <img src={logo1000} alt="logo" className="logo" />
-
+      <img src="src/assets/LD_Logo_1000px.png" alt="logo" className="logo" />
       <h2>About The Language Dungeon</h2>
       <div className="paragraph-container">
         <p>
@@ -48,7 +41,6 @@ export default function SplashPage() {
           </span>
         </p>
       </div>
-
       <div className="signup-container">
         <h2>Get Started</h2>
         <p>Please create a <b>Username</b> and <b>Password</b> to begin your Language Learning Journey!</p>
@@ -64,14 +56,13 @@ export default function SplashPage() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        <AvatarSelection setAvatar={setAvatar} />
         <button onClick={handleSignUpClick}>Sign Up Now</button>
       </div>
-
+      {error && <div>Signup failed: {error.message}</div>}
       <footer>
         &copy; 2023 The Language Dungeon. All rights reserved.
       </footer>
     </div>
-      )}
-    </>
   );
 }
